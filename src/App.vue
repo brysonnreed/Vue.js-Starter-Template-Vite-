abd7c45c893166d3bf0c1da1f39e8455a50511f1
Bryson Reed <brysonjreed@gmail.com>
2024-02-09 19:21:50 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- Delete -->
+  <h1>
+    <span>{{ framework }}</span> Base Starter With <span>{{ bundler }}</span>
+  </h1>
+  <a
+    href="https://github.com/brysonnreed?tab=repositories"
+    target="_blank"
+    aria-label="Link to my other Github repositories"
+    class="btn"
+    >My Github Repos
+  </a>
+  <!-- Delete -->
+</template>
+
+<script setup>
+import "./assets/main.css";
+const framework = "Vue.js";
+const bundler = "Vite";
+</script>
